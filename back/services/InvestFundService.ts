1a3481b476a538e90ec58cde5019c5e562a008c1
maxdlr <contact@maxdlr.com>
2025-06-23 23:50:10 +0200

    feat: find valo by fund isin

--- back/services/InvestFundService.ts
+++ back/services/InvestFundService.ts
@@ -26,8 +26,3 @@ export const deposit = (req: DepositRequestInterface): string | boolean => {
   }
   return true;
 };
-
-// 1. Montant (en €, required)
-// 2. Un RIB + BIC
-// 3. Le choix des fonds avec la répartition (cf le fichier de ressource `fakeFunds.json` attaché plus bas. Tu y trouveras 10 fonds d’investissement fictifs, avec 2 mois de valorisations boursières).
-// 4. L’allocation de son versement sur les fonds choisis. Une fois le choix des fonds fait, la personne peut faire une **allocation, ie répartir son versement en N% sur les fonds sélectionnés.** Exemple, si elle choisit de déposer 100€ sur 4 fonds, elle peut décider de déposer 3% sur le fonds A, 27% sur le fonds B, 50% sur le fonds C et 20% sur le fonds D, etc.
